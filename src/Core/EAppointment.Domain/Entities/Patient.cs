2d4656a909124ca560e89c371d0596b6b97eb4bb
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-30 23:47:52 +0300

    feat Add Patient entity

---
+++ src/Core/EAppointment.Domain/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿namespace EAppointment.Domain.Entities
+{
+    public class Patient
+    {
+        public Patient() => Id = Guid.NewGuid();
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string IdentityNumber { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
+        public string FullAddress { get; set; } = string.Empty;
+    }
+}
