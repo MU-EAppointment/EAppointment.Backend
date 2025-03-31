30adf819211a84d38d8a3ebe23856d8ca8d83539
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-31 23:51:20 +0300

    feat: Add IQueryRepository, IRepository, and ICommandRepository interfaces

---
+++ src/Core/EAppointment.Application/Abstractions/Repositories/Commons/ICommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using EAppointment.Domain.Entities.Commons;
+
+namespace EAppointment.Application.Abstractions.Repositories.Commons
+{
+    public interface ICommandRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(int id);
+        Task SaveAsync();
+    }
+}
---
+++ src/Core/EAppointment.Application/Abstractions/Repositories/Commons/IQueryRepository.cs
@@ -0,0 +1,10 @@
+﻿using EAppointment.Domain.Entities.Commons;
+
+namespace EAppointment.Application.Abstractions.Repositories.Commons
+{
+    public interface IQueryRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+    }
+}
