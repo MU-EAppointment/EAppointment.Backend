40f798274b221252ff71e773746999a8d052a1e2
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-03 21:37:50 +0300

    feat: Add BaseException and ErrorType for centralized error handling

---
+++ src/Core/EAppointment.Application/Commons/Exceptions/ErrorType.cs
@@ -0,0 +1,14 @@
+﻿using EAppointment.Application.Abstractions.Commons.Exceptions;
+
+namespace EAppointment.Application.Commons.Exceptions
+{
+    public readonly record struct ErrorType(byte Code, string Message) : IErrorType
+    {
+        public override string ToString() => $"{Code}: {Message}";
+
+        public static readonly ErrorType NotFound = new(001, "Not Found");
+        public static readonly ErrorType Unauthorized = new(002, "Unauthorized");
+        public static readonly ErrorType BadRequest = new(003, "Bad Request");
+        public static readonly ErrorType InternalServer = new(004, "Bad Request");
+    }
+}
