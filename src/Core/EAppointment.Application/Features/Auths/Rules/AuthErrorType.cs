48293414c06bb2d562a4eb7895dc3c42b48cf8e8
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-03 21:41:44 +0300

    feat: Add AuthRules for authentication validation and AuthErrorType for error handling

---
+++ src/Core/EAppointment.Application/Features/Auths/Rules/AuthErrorType.cs
@@ -0,0 +1,11 @@
+﻿using EAppointment.Application.Abstractions.Commons.Exceptions;
+using EAppointment.Application.Commons.Exceptions;
+
+namespace EAppointment.Application.Features.Auths.Rules
+{
+    public readonly record struct AuthErrorType(byte Code, string Message) : IErrorType
+    {
+        public static readonly ErrorType UserNotFound = new(010, "User Not Found");
+        public static readonly ErrorType PasswordNotCorrect = new(011, "Password Not Correct");
+    }
+}
---
+++ src/Core/EAppointment.Application/Features/Auths/Rules/AuthRules.cs
@@ -0,0 +1,24 @@
+﻿using EAppointment.Application.Commons.Exceptions;
+using EAppointment.Application.Commons.Results;
+using EAppointment.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace EAppointment.Application.Features.Auths.Rules
+{
+    internal readonly struct AuthRules(UserManager<User> _userManager)
+    {
+        internal async Task<User> UserNotFound(string emailOrUsername, CancellationToken cancellationToken)
+        {
+            User? user = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == emailOrUsername || u.Email == emailOrUsername, cancellationToken);
+            return user ?? throw new BaseException(AuthErrorType.UserNotFound, HttpStatusCode.NotFound);
+        }
+
+        internal async Task IsPasswordCorrect(User user, string password)
+        {
+            bool data = await _userManager.CheckPasswordAsync(user, password);
+            if (data is false) throw new BaseException(AuthErrorType.PasswordNotCorrect, HttpStatusCode.NotFound);
+        }
+    }
+}
