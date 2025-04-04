3d0f9997a5be299b341e673f455dc39aca2db4dd
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-04 19:54:28 +0300

    feat: Implement JwtToken structure and register it in DI container

---
+++ src/Infrastructure/EAppointment.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using EAppointment.Application.Abstractions.Services;
+using EAppointment.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EAppointment.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services) 
+        {
+            services.AddScoped<IJwtTokenHandler, JwtTokenHandler>();
+            return services;
+        }
+    }
+}
