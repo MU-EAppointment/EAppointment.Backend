cec33a2a099f5d8e64e2c0ef35758fa29060ae47
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-04 10:53:06 +0300

    feat: Add EAppointmentDbContext with Identity support and configure Identity services

---
+++ src/Infrastructure/EAppointment.Persistence/Contexts/EAppointmentDbContext.cs
@@ -0,0 +1,10 @@
+﻿using EAppointment.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EAppointment.Persistence.Contexts
+{
+    internal sealed class EAppointmentDbContext(DbContextOptions options) : IdentityDbContext<User, Role, Guid>(options)
+    {
+    }
+}
--- src/Infrastructure/EAppointment.Persistence/ServiceRegistration.cs
+++ src/Infrastructure/EAppointment.Persistence/ServiceRegistration.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using EAppointment.Domain.Entities;
+using EAppointment.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EAppointment.Persistence
 {
     public static class ServiceRegistration
     {
-        public static IServiceCollection AddPersistenceServices(this IServiceCollection services) 
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration _configuration) 
         {
+            services.AddDbContext<EAppointmentDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("SQL")));
+            services.AddIdentity<User, Role>(action =>
+            {
+                action.Password.RequiredLength = 1;
+                action.Password.RequireUppercase = false;
+                action.Password.RequireLowercase = false;
+                action.Password.RequireNonAlphanumeric = false;
+                action.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<EAppointmentDbContext>();
             return services;
         }
     }
