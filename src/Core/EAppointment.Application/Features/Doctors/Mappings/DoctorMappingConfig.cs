7e64fce2d1208ed75cc0e72a45c968de78f7a194
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-08 23:19:22 +0300

    feat: Implement GetAll endpoint for doctors and return list from database

---
+++ src/Core/EAppointment.Application/Features/Doctors/Mappings/DoctorMappingConfig.cs
@@ -0,0 +1,15 @@
+﻿using EAppointment.Application.Features.Doctors.DTOs;
+using EAppointment.Domain.Entities;
+using Mapster;
+
+namespace EAppointment.Application.Features.Doctors.Mappings
+{
+    internal sealed class DoctorMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Doctor, GetAllDoctorDTO>()
+                .Map(dest => dest.Department, src => src.Department);
+        }
+    }
+}
---
+++ src/Presentation/EAppointment.WebAPI/Controllers/DoctorsController.cs
@@ -0,0 +1,20 @@
+﻿using EAppointment.Application.Commons.Results;
+using EAppointment.Application.Features.Auths.Commands.Login;
+using EAppointment.Application.Features.Auths.DTOs;
+using EAppointment.Application.Features.Doctors.Queries.GetAll;
+using EAppointment.WebAPI.Abstractions;
+using Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EAppointment.WebAPI.Controllers
+{
+    public sealed class DoctorsController(IMediator _mediator) : ApiController(_mediator)
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll(GetAllDoctorQueryRequest getAllDoctorQueryRequest, CancellationToken cancellationToken)
+        {
+            Result<List<Application.Features.Doctors.DTOs.GetAllDoctorDTO>> response = await _mediator.Send(getAllDoctorQueryRequest, cancellationToken);
+            return StatusCode((int)response.HttpStatusCode, response);
+        }
+    }
+}
