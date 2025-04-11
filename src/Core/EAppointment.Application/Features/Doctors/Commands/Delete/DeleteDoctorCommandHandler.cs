ca7a3d91e63243edb0ca1785f280566a1a0c06c9
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-04-11 22:55:10 +0300

    feat: Implement Doctor delete with API integration and user confirmation

---
+++ src/Core/EAppointment.Application/Features/Doctors/Commands/Delete/DeleteDoctorCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using EAppointment.Application.Abstractions.Repositories;
+using EAppointment.Application.Commons.Results;
+using EAppointment.Application.Features.Doctors.DTOs;
+using EAppointment.Domain.Entities;
+using Mediator;
+
+namespace EAppointment.Application.Features.Doctors.Commands.Delete
+{
+    public readonly record struct DeleteDoctorCommandRequest(Guid id) : IRequest<Result<string>>;
+    internal sealed class DeleteDoctorCommandHandler(ICommandRepository<Doctor> _doctorCommandRepository) : IRequestHandler<DeleteDoctorCommandRequest, Result<string>>
+    {
+        public async ValueTask<Result<string>> Handle(DeleteDoctorCommandRequest request, CancellationToken cancellationToken)
+        {
+            await _doctorCommandRepository.DeleteAsync(request.id);
+            await _doctorCommandRepository.SaveAsync();
+
+            return Result<string>.Success("Doctor deleted successfully.");
+        }
+    }
+}
